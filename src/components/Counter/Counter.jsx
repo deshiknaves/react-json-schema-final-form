af77405bca529a3b72706f13c822a080c7cdf9c4
Deshi Rahim <nawal.rahim@gmail.com>
2020-05-01 22:11:11 +1000

    ✨ Added test component

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export const Counter = () => <p>Counter: 0</p>
---
+++ stories/Counter.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Counter } from '../src/components/Counter'
+
+export const basicExample = () => <Counter />
+
+export default {
+  title: 'Counter',
+  component: Counter,
+}
