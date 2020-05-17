ff20691a6406579cf3d9cf7a3855d2518eeccf7d
Deshi Rahim <nawal.rahim@gmail.com>
2020-05-17 11:29:19 +1000

    🔧 Update jest lint config

--- test/jest.lint.js
+++ test/jest.lint.js
@@ -1,9 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 
 module.exports = {
   rootDir: path.join(__dirname, '..'),
   displayName: 'lint',
   runner: 'jest-runner-eslint',
-  testMatch: ['<rootDir>/**/*.{js|jsx|tsx|ts}'],
+  testMatch: ['<rootDir>/**/*.(js|ts|tsx|jsx)'],
   testPathIgnorePatterns: ['/node_modules/', '/coverage/', '/dist/', '/other/'],
 }
